3f375f027dc13d3f07e0466ac0210bd821e8c580
Adrián Insua Yañez <adrianiy@ext.inditex.com>
2020-04-14 18:14:23 +0200

    test trusty

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -24,7 +24,7 @@ function Login(props) {
             event.preventDefault();
         }
         try {
-            const drv = driver(uri, auth.basic(user, password), { encrypted: "ENCRYPTION_ON" });
+            const drv = driver(uri, auth.basic(user, password), { encrypted: "ENCRYPTION_ON", trust: "TRUST_ALL_CERTIFICATES" });
             if (drv._authToken) {
                 saveCredentials();
                 props.callback({ drv, user });
