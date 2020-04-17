3446664a7c75132a680479896fc49cdea6a6eb37
Adrian Insua <adrian.insua.y@gmail.com>
2020-04-17 14:26:58 +0200

    Bugfix/#11 (#12)
    * [#11] fix results parsing
    
    * [#11] update changelog
    
    Co-authored-by: Adrián Insua Yañez <adrianiy@ext.inditex.com>

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -5,14 +5,17 @@ import { getChart } from '../../service/neo.service';
 
 function Chart(props) {
     const [results, setResults] = useState(null);
+    const [updated, setUpdated] = useState(null);
 
     const fecthData = useCallback(async () => {
         const results = await getChart(props.sessionId, props.query);
         console.log(results);
         setResults(results);
+        setUpdated(new Date().getTime());
     }, [props])
 
     useEffect(() => {
+        setResults(null);
         fecthData();
     }, [props, fecthData])
 
@@ -25,14 +28,15 @@ function Chart(props) {
     }
 
     return results ? (
-        <Visualization
-            style={{width: '100%'}}
-            result={results}
-            maxNeighbours={ 30 }
-            sessionId={ props.sessionId }
-            itemHovered={itemHover}
-            itemSelected={itemSelected}
-        />
+            <Visualization
+                style={{width: '100%'}}
+                result={results}
+                maxNeighbours={ 30 }
+                sessionId={ props.sessionId }
+                itemHovered={itemHover}
+                itemSelected={itemSelected}
+                updated={updated}
+            />
     ) :  null
 }
 
