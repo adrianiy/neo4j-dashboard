b1a3d3a9d45c77b2bb1614a2ca0982672f4d04a1
Adrián Insua Yañez <adrianiy@ext.inditex.com>
2020-04-24 01:47:37 +0200

    style: fix lint errors

--- src/assets/visualization/utils/circumferentialRelationshipRouting.js
+++ src/assets/visualization/utils/circumferentialRelationshipRouting.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 /*
  * Copyright (c) 2002-2020 "Neo4j,"
  * Neo4j Sweden AB [http://neo4j.com]
--- src/global/utils/hooks/theme.js
+++ src/global/utils/hooks/theme.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, useState } from "react"
+import React from "react"
 
 export const themes = {
     light: {
